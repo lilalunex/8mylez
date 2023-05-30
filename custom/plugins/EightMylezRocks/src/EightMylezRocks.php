1b940090055ca6379ebd59de040dc97c23debca8
lilalunex <alexanderscharow@icloud.com>
2023-05-30 17:33:51 +0200

    🚀 first commit

---
+++ custom/plugins/EightMylezRocks/src/EightMylezRocks.php
@@ -0,0 +1,10 @@
+<?php declare(strict_types=1);
+
+namespace EightMylezRocks;
+
+use Shopware\Core\Framework\Plugin;
+use Shopware\Storefront\Framework\ThemeInterface;
+
+class EightMylezRocks extends Plugin implements ThemeInterface
+{
+}
