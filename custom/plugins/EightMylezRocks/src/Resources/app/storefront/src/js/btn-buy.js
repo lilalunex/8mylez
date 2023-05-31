ca8a73ce5f87b6776d4224859b669b604358669e
lilalunex <alexanderscharow@icloud.com>
2023-05-31 11:50:33 +0200

    ⚒️ saving progress

---
+++ custom/plugins/EightMylezRocks/src/Resources/app/storefront/src/js/btn-buy.js
@@ -0,0 +1,8 @@
+import Plugin from 'src/plugin-system/plugin.class'
+
+export default class ButtonBuy extends Plugin {
+	init() {
+		console.info("Gotcha")
+		console.log("Gotcha")
+	}
+}
---
+++ custom/plugins/EightMylezRocks/src/Resources/app/storefront/src/main.js
@@ -0,0 +1,5 @@
+import ButtonBuy from "./js/btn-buy";
+
+window.PluginManager.register('ButtonBuy', ButtonBuy, '.main-navigation')
+
+console.log('main.js')
